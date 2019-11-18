aa12a5ee68bf9e70a010917def1b99d3426c4266
jpine01 <57878010+jpine01@users.noreply.github.com>
2019-11-17 20:03:23 -0500

    Add files via upload

---
+++ Prog4/Prog1A/DescendingParcelZip.cs
@@ -0,0 +1,30 @@
+﻿// Program 4
+// CIS 200-01
+// Fall 2018
+// Due: 11/26/2018
+// GradeID:  D9817
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prog1
+{
+    class DescendingParcelZip : Comparer<Parcel>
+    {
+        // Precondition: Two parcel class objects need to be passed in
+        // Postcondition: Turns to descending order by destination zipcode
+        public override int Compare(Parcel p1, Parcel p2)
+        {
+            if (p1 == null && p2 == null)
+                return 0;
+            if (p1 == null)
+                return -1;
+            if (p2 == null)
+                return 1;
+
+            return (-1) * p1.DestinationAddress.Zip.CompareTo(p2.DestinationAddress.Zip);
+        }
+    }
+}
---
+++ Prog4/Prog1A/TestParcels.cs
@@ -0,0 +1,104 @@
+﻿// Program 4
+// CIS 200-01
+// Fall 2018
+// Due: 11/26/2018
+// GradeID:  D9817
+
+// File: TestParcels.cs
+// This is a simple, console application designed to exercise the Parcel hierarchy.
+// It creates several different Parcels and prints them.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prog1
+{
+    class TestParcels
+    {
+        // Precondition:  None
+        // Postcondition: Parcels have been created and displayed
+        static void Main(string[] args)
+        {
+            // Test Data - Magic Numbers OK
+            Address a1 = new Address("  John Smith  ", "   123 Any St.   ", "  Apt. 45 ",
+                "  Louisville   ", "  KY   ", 40202); // Test Address 1
+            Address a2 = new Address("Jane Doe", "987 Main St.",
+                "Beverly Hills", "CA", 90210); // Test Address 2
+            Address a3 = new Address("James Kirk", "654 Roddenberry Way", "Suite 321",
+                "El Paso", "TX", 79901); // Test Address 3
+            Address a4 = new Address("John Crichton", "678 Pau Place", "Apt. 7",
+                "Portland", "ME", 04101); // Test Address 4
+            Address a5 = new Address("John Doe", "111 Market St.", "",
+                "Jeffersonville", "IN", 47130); // Test Address 5
+            Address a6 = new Address("Jane Smith", "55 Hollywood Blvd.", "Apt. 9",
+                "Los Angeles", "CA", 90212); // Test Address 6
+            Address a7 = new Address("Captain Robert Crunch", "21 Cereal Rd.", "Room 987",
+                "Bethesda", "MD", 20810); // Test Address 7
+            Address a8 = new Address("Vlad Dracula", "6543 Vampire Way", "Apt. 1",
+                "Bloodsucker City", "TN", 37210); // Test Address 8
+
+            Letter letter1 = new Letter(a1, a2, 3.95M);                            // Letter test object
+            GroundPackage gp1 = new GroundPackage(a3, a4, 14, 10, 5, 12.5);        // Ground test object
+            NextDayAirPackage ndap1 = new NextDayAirPackage(a1, a3, 25, 15, 15,    // Next Day test object
+                85, 7.50M);
+            TwoDayAirPackage tdap1 = new TwoDayAirPackage(a4, a1, 46.5, 39.5, 28.0, // Two Day test object
+                80.5, TwoDayAirPackage.Delivery.Saver);
+            Letter letter2 = new Letter(a5, a6, 2.99m);
+            GroundPackage gp2 = new GroundPackage(a7, a8, 9, 11, 7, 10.99);
+            NextDayAirPackage ndap2 = new NextDayAirPackage(a3, a7, 39, 13, 9, 92, 4.85M);
+            TwoDayAirPackage tdap2 = new TwoDayAirPackage(a8, a1, 56.2, 28.7, 30, 79.4, TwoDayAirPackage.Delivery.Early);
+            Letter letter3 = new Letter(a4, a6, 5.67m);
+            Letter letter4 = new Letter(a2, a7, 4.99m);
+
+            List<Parcel> parcels;      // List of test parcels
+
+            parcels = new List<Parcel>();
+
+            parcels.Add(letter1); // Populate list
+            parcels.Add(gp1);
+            parcels.Add(ndap1);
+            parcels.Add(tdap1);
+            parcels.Add(letter2);
+            parcels.Add(gp2);
+            parcels.Add(ndap2);
+            parcels.Add(tdap2);
+            parcels.Add(letter3);
+            parcels.Add(letter4);
+
+            parcels.Sort();
+
+            Console.WriteLine("Natural Order:");
+            Console.WriteLine("====================");
+            foreach (Parcel p in parcels)
+            {
+                Console.WriteLine(p);
+                Console.WriteLine("====================");
+            }
+            Pause();
+
+            parcels.Sort(new DescendingParcelZip());
+
+            Console.WriteLine("Sorted list (descending by destination zip) using Comparer:");
+            Console.WriteLine("====================");
+            foreach (Parcel p in parcels)
+            {
+                Console.WriteLine(p);
+                Console.WriteLine("====================");
+            }
+            Pause();
+        }
+
+        // Precondition:  None
+        // Postcondition: Pauses program execution until user presses Enter and
+        //                then clears the screen
+        public static void Pause()
+        {
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+
+            Console.Clear(); // Clear screen
+        }
+    }
+}
